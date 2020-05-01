5d3ecc3cbfc727e68105a642f36e3154c68d45f9
Ann <Annkumaneva@yandex.ru>
2020-05-01 14:13:57 +0300

    Add Task2  - collection and sort

---
+++ MyHomeWork/MyHomeWork/Book.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHomeWork
+{
+    class Book : Printed_Product, IPublishable
+    {
+        private int printYear;
+        private int printMounth;
+        private int printDay;
+
+        public string Author { get; set; }
+        public string Genre { get; set; }
+
+        public Book (string genre, string title, string author)
+        {
+            this.Genre = genre;
+            Title = title;
+            this.Author = author;
+        }
+        public override void MarkRead()
+        {
+            IsRead = true;
+        }
+
+        public override void PrintName()
+        {
+            Console.WriteLine(Genre + " " + Title + " " + Author);
+        }
+
+        public void Publish()
+        {
+            Console.Write("Введите год издания:");
+            printYear = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите месяц издания:");
+            printMounth = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите день издания:");
+            printDay = int.Parse(Console.ReadLine());
+
+            Console.Write(Genre + " " + Title + " " + Author + " напечатан(а): ");
+
+            Console.Write(printDay + "." + printMounth + "." + printYear);
+            Console.WriteLine();
+        }
+
+        private static void PrintStatus(Book book1)
+        {
+            if (book1.IsRead)
+            {
+                Console.WriteLine("Статус - прочитан(а)");
+            }
+            else
+            {
+                Console.WriteLine("Статус - не прочитан(а)");
+            }
+        }
+    }
+}
